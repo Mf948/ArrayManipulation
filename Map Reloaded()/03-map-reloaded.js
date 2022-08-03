a4a85e316e19aa3449d12d7551a622e2f6a81a68
Mateo Ferreira <Matteo948@icloud.com>
2022-08-02 20:23:57 -0400

    map Reloaded()

---
+++ Map Reloaded()/03-map-reloaded.js
@@ -0,0 +1,42 @@
+const orders =[
+{
+    costumerName: 'Nicolas',
+    total: 60,
+    delivered: true,
+},{
+    costumerName: 'Zulema',
+    total: 120,
+    delivered: false,
+},{
+    costumerName: 'santiago',
+    total: 180,
+    delivered: true,
+},{
+    costumerName: 'valentina',
+    total: 240,
+    delivered: true,
+},
+
+];
+console.log("original",orders);
+
+const rta = orders.map(item => item.total );// transform only total array
+
+console.log('rta',rta);
+
+const rta2 = orders.map(item => {
+    item.tax = .19; /// added new atrribute tax: 0.19
+    return item// changes the satate of the object in the memory.. 
+})
+
+console.log('rta2',rta2);
+
+
+const rta2 = orders.map(item => {
+    return {
+        ...item, // new object and 
+        tax: 19 //new reference in memory added to the new object
+    };
+});
+
+console.log('rta3',rta3);
