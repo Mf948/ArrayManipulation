4438273b3276d404f5a1ce1b16a122bf8916fa58
Mateo Ferreira <Matteo948@icloud.com>
2022-08-11 21:17:15 -0400

    reduced reloaded

--- reduce()/index.js
+++ reduce()/index.js
@@ -1,1 +1,18 @@
-z
+const totals = [1,2,3,4];
+
+
+let sum = 0;
+
+for (let index = 0 ; index< totals.length; index++){
+    const elememt = totals[index]; 
+    sum = sum+ elememt
+
+}
+
+console.log(sum)
+
+
+
+const rta = totals.reduce((sum, element)=>sum + element, 10);
+
+console.log('rta', rta)
