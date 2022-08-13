c0a2c27303a78798941493adba3d3b6cf62b5ad8
Mateo Ferreira <Matteo948@icloud.com>
2022-08-12 21:20:43 -0400

    every()

--- some()/07-some().js
+++ some()/07-some().js
@@ -17,99 +17,80 @@ console.log(rta);
 
 const rta2 = numbers.some(item => item % 2 ===0);
 
-console.log(rta,'rta2'); 
+console.log(rta2,'rta3'); 
 
 
 
-
-
-
-
-//  const rta2 = numbers.some(item => item % 2 === 0);
-
-//  console.log(rta2, 'rta2'); 
-
-//  const orders =[
-//     {
-//         costumerName: 'Nicolas',
-//         total: 60,
-//         delivered: true,
-//     },{
-//         costumerName: 'Zulema',
-//         total: 120,
-//         delivered: false,
-//     },{
-//         costumerName: 'santiago',
-//         total: 180,
-//         delivered: true,
-//     },{
-//         costumerName: 'valentina',
-//         total: 240,
-//         delivered: true,
-//     },
+const orders =[
+    {
+        costumerName: 'Nicolas',
+        total: 60,
+        delivered: true,
+    },{
+        costumerName: 'Zulema',
+        total: 120,
+        delivered: false,
+    },{
+        costumerName: 'santiago',
+        total: 180,
+        delivered: true,
+    },{
+        costumerName: 'valentina',
+        total: 240,
+        delivered: true,
+    },
     
-//     ];
-
-//     const rta3 = orders.some(item => item % 2 === 0);
-// console.log(rta3,'ss')
+    ]; 
 
 
 
-// const dates = [
-//     {
-//       startDate: new Date(2021, 1, 1, 10),
-//       endDate: new Date(2021, 1, 1, 11),
-//       title: "Cita de trabajo",
-//     },
-//     {
-//       startDate: new Date(2021, 1, 1, 15),
-//       endDate: new Date(2021, 1, 1, 15, 30),
-//       title: "Cita con mi jefe",
-//     },
-//     {
-//       startDate: new Date(2021, 1, 1, 20),
-//       endDate: new Date(2021, 1, 1, 21),
-//       title: "Cena",
-//     },
-//   ];
+    const rta3 = orders.some(item => item.delivered);
+
+    console.log(rta3,'rta2'); 
 
 
 
 
 
+const dates = [
+    {
+      startDate: new Date(2021, 1, 1, 10),
+      endDate: new Date(2021, 1, 1, 11),
+      title: "work",
+    },
+    {
+      startDate: new Date(2021, 1, 1, 15),
+      endDate: new Date(2021, 1, 1, 15, 30),
+      title: "meeting with boss",
+    },
+    {
+      startDate: new Date(2021, 1, 1, 20),
+      endDate: new Date(2021, 1, 1, 21),
+      title: "dinner",
+    },
+  ];
 
-//   const newAppointment = {
-//       startDate: new Date(2021, 1, 1, 8),
-//       endDate: new Date(2021, 1, 9, 30), 
-//   }
 
-//   var getOverlappingDaysInIntervals = require('date-fns/getOverlappingDaysInIntervals')
+  const newAppointment = {
+      startDate: new Date(2021, 1, 1, 8),
+      endDate: new Date(2021, 1, 9, 30), 
+  }
 
 
-// const isOverlap = (newDate) => {
-//     return dates.some(date => {
-//         return getOverlappingDaysInIntervals(
-//             {start: date.startDate, end: date.endDate},
-//             {start: date.startDate, end: date.endDate})
+  var getOverlappingDaysInIntervals = require('date-fns/getOverlappingDaysInIntervals')
 
-//     })
 
+const isOverlap = (newDate) => {
+    return dates.some(date => {
+        return getOverlappingDaysInIntervals(
+            {start: date.startDate, end: date.endDate},
+            {start: newDate.startDate, end: date.endDate},
+            
+        )
+    })
  
- 
-// }
+}
 
-// console.log(isOverlap(newAppointment),"overlap");
-// const numbers = [ 1,2,3,4];
+console.log(isOverlap(newAppointment),"overlap");
 
-// let rta = false;
 
-// for (let index = 0; index < numbers.length; index++){
-//     const element = number[index];
-//     if(element % 2 === 0){ // even Number
-//  rta = true;
-//  break;
-
-//     }
-// }
-
-// console.log(rta)
